fd0cca49fcf91a5cff4e06650f350848649ef573
maming1995 <32721696+maming1995@users.noreply.github.com>
2017-12-06 11:40:03 -0800

    Add files via upload

---
+++ UNO_Generate_signal.ino
@@ -0,0 +1,25 @@
+
+int SPEAKER = 12;    // The speaker is on pin 12
+
+int freq = 10;      // Starting frequency
+
+void setup()
+{
+    pinMode(SPEAKER, OUTPUT);
+}
+
+void loop()
+{
+    freq += 50;      // add 50 to freq
+
+    if (freq >30000)
+    {
+        noTone(SPEAKER);
+        freq = 10;
+    }
+
+    tone(SPEAKER, freq);
+
+    delay(100);
+}
+
